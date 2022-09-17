90a4f3cc063156b40ae43edf3491b0440499e355
wsetws <wsetws@gmail.com>
2022-09-16 22:51:12 -0300

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 app.get("/",function(req, res) {
-    res.send('Hello Express');
+    res.sendFile(__dirname + "/views/index.html");
   });
 console.log("Hello World");
 
